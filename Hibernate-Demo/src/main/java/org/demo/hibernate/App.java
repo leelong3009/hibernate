0837e35df130969244c4789607ac5be2572182e9
Lee Long <longlh3009@gmail.com>
2015-07-08 11:25:24 +0800

    init

--- Hibernate-Demo/src/main/java/org/demo/hibernate/App.java
+++ Hibernate-Demo/src/main/java/org/demo/hibernate/App.java
@@ -1,5 +1,17 @@
 package org.demo.hibernate;
 
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.demo.hibernate.dto.Address;
+import org.demo.hibernate.dto.UserDetails;
+import org.demo.hibernate.dto.UserDetails_OnetoOne;
+import org.demo.hibernate.dto.Vehicle;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 /**
  * Hello world!
  *
@@ -8,6 +20,46 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+//        UserDetails user = new UserDetails();
+//        user.setUserId(1);
+//        user.setUserName("First User");
+//        user.setJoinedDate(new Date());
+//        user.setDescription("this is the first user");
+        
+        
+//        Address address = new Address();
+//        address.setCity("Singapore");
+//        address.setPinCode("1234");
+//        address.setState("state");
+//        address.setStreet("bedok");
+//        user.setHomeAddress(address);
+//        
+//        Address address2 = new Address();
+//        address2.setCity("Singapore");
+//        address2.setPinCode("1234");
+//        address2.setState("state 2");
+//        address2.setStreet("bedok 2");
+//        user.setOfficeAddress(address2);
+//        
+//        user.getListOfAddress().add(address);
+//        user.getListOfAddress().add(address2);
+        
+    	Vehicle vehicle = new Vehicle();
+        vehicle.setVehicleName("Car");
+        
+        UserDetails_OnetoOne user = new UserDetails_OnetoOne();
+        user.setUserName("First User");
+        user.setVehicle(vehicle);
+        
+//      create session factory
+        SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        session.save(user);
+        session.save(vehicle);
+        session.getTransaction().commit();
+        session.close();
+        
+        System.exit(1);
     }
 }
--- Hibernate-Demo/src/main/java/org/demo/hibernate/dto/UserDetails.java
+++ Hibernate-Demo/src/main/java/org/demo/hibernate/dto/UserDetails.java
@@ -1,14 +1,97 @@
 package org.demo.hibernate.dto;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.AttributeOverride;
+import javax.persistence.AttributeOverrides;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.Lob;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Transient;
+
+import org.hibernate.annotations.CollectionId;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
+
 
 @Entity
+@Table(name="USER_DETAILS")
 public class UserDetails {
 	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int userId;
+	
+//	fetch: Defines whether the value of the field or property should be lazily loaded or must be eagerly fetched.
+//	optional : Defines whether the value of the field or property may be null.
+	@Basic(fetch=FetchType.LAZY, optional=true)
 	private String userName;
 	
+	@Temporal(TemporalType.DATE)
+	private Date joinedDate;
+	
+//	DEMO override attribute
+	@Embedded
+	@AttributeOverrides({
+		@AttributeOverride(name="street", column=@Column(name="HOME_STREET_NAME")),
+		@AttributeOverride(name="city", column=@Column(name="HOME_CITY_NAME")),
+		@AttributeOverride(name="state", column=@Column(name="HOME_STATE_NAME")),
+		@AttributeOverride(name="pinCode", column=@Column(name="HOME_PINCODE_NAME"))
+	})
+	private Address homeAddress;
+	
+	@Embedded
+	private Address officeAddress;
+	
+	@Lob
+	private String description;
+	
+//	Specifies that the property or field is not persistent.
+	@Transient
+	private String redundantValue;
+	
+	@ElementCollection(fetch=FetchType.EAGER)
+	@JoinTable(name="USER_ADDRESS",
+				joinColumns=@JoinColumn(name="USER_ID")
+			)
+//	@GenericGenerator(name = "hilo-gen", strategy = "hilo")
+//	@CollectionId(columns = { @Column(name="ADDRESS_ID") }, generator = "hilo-gen", type = @Type(type = "long"))
+	private Collection<Address> listOfAddress = new ArrayList<Address>();
+	
+	public Date getJoinedDate() {
+		return joinedDate;
+	}
+	public void setJoinedDate(Date joinedDate) {
+		this.joinedDate = joinedDate;
+	}
+
+	public String getRedundantValue() {
+		return redundantValue;
+	}
+	public void setRedundantValue(String redundantValue) {
+		this.redundantValue = redundantValue;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
 	public int getUserId() {
 		return userId;
 	}
@@ -20,6 +103,24 @@ public class UserDetails {
 	}
 	public void setUserName(String userName) {
 		this.userName = userName;
+	}
+	public Address getHomeAddress() {
+		return homeAddress;
+	}
+	public void setHomeAddress(Address homeAddress) {
+		this.homeAddress = homeAddress;
+	}
+	public Address getOfficeAddress() {
+		return officeAddress;
+	}
+	public void setOfficeAddress(Address officeAddress) {
+		this.officeAddress = officeAddress;
+	}
+	public Collection<Address> getListOfAddress() {
+		return listOfAddress;
+	}
+	public void setListOfAddress(Collection<Address> listOfAddress) {
+		this.listOfAddress = listOfAddress;
 	}
 	
 	
